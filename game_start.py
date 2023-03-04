6ffce331af8e080a4ffe92d3233c86b9cd414d4d
Mantas <62988421+Garfas@users.noreply.github.com>
2023-03-04 23:39:08 +0200

    editing

--- game_start.py
+++ game_start.py
@@ -1,21 +1,16 @@
-# sukursime klausimų klasę, kuri atspindės kiekvieną žaidimo klausimą. 
-# Kiekvienas klausimas turės raginimą ir galimų atsakymų rinkinį, 
-# o vienas iš jų bus teisingas.
-
-
-
-
-# Tada sukursime modulį, vadinamą klausimais, kad išsaugotume 
-# klausims, kurie bus naudojami žaidime, sąrašą.
-
 from questions import *
-
-#Tada sukursime žaidimų klasę, kad galėtume valdyti patį žaidimą. Ši klasė turės metodus, kaip pradėti žaidimą, užduoti klausimus ir sekti vartotojo rezultatą.
 import random
 import logging
 from typing import List
 
 logging.basicConfig(level=logging.INFO)
+
+
+#Tada sukursime žaidimų klasę, kad galėtume valdyti patį žaidimą. Ši klasė turės metodus, kaip pradėti žaidimą, užduoti klausimus ir sekti vartotojo rezultatą.
+
+#///// English
+#Then we'll create a game class to handle the game itself. This class will have methods to start the game, ask questions, and track the user's score.
+
 
 class Game:
     def __init__(self, questions) -> None:
@@ -51,13 +46,27 @@ class Game:
             logging.info(f"\n'{user_answer}' is not a valid option! Try again")
             return self.ask_question(number, question)
 
-# Galiausiai galime sukurti pagrindinę žaidimo pradžios funkciją, sukurdami Žaidimo objektą su klausimais iš klausimų modulio.
+
+# Galiausiai sukuriam pagrindinę žaidimo pradžios funkciją, sukurdami Žaidimo objektą su klausimais iš klausimų modulio.
+
+#////English
+#Finally, we create the main game launch function by creating a Game object with questions from the questions module.
+
 
 from game_start import Game
 
 def main():
     game = Game(questions)
-    game.start()
+
+    while True:
+        game.start()
+
+        logging.info("Nice try! Enter 'Y' to play again. Enter any other value to exit")
+        play_again_answer = input("Play again: ").upper()
+        # logging.info(play_again_answer, [play_again_answer != 'Y'])
+        if play_again_answer != "Y":
+            break
 
 if __name__ == '__main__':
     main()
+
